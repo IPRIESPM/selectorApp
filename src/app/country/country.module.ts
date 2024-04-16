005985ddeff25623b7e0131a954d5b45723198e9
Isaac <105789412+altaskur@users.noreply.github.com>
2024-04-16 10:42:28 +0200

    feat: :sparkles: add interfaces, services and dynamic form components

--- src/app/country/country.module.ts
+++ src/app/country/country.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SelectorPageComponent } from './pages/selector-page/selector-page.component';
 import { CountryRoutingModule } from './country-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [SelectorPageComponent],
-  imports: [CommonModule, CountryRoutingModule],
+  imports: [CommonModule, CountryRoutingModule, ReactiveFormsModule],
   exports: [],
 })
 export class CountryModule {}
